178f8ad0ce7763634702078659ee6f4817ae11b5
claresinger <csinger@caltech.edu>
2020-07-08 23:26:24 -0700

    add content to tests

--- test/test_insolation.jl
+++ test/test_insolation.jl
@@ -10,41 +10,44 @@ tz = PST
 lon, lat = California
 
 date = Dates.DateTime(2020,3,21,12,0,0)
-S = instantaneous_insolation(date, tz, lon, lat)
-@test S ≈ 25.0 rtol=rtol
 
-S = daily_insolation(date, lat)
-@test S ≈ 25.0 rtol=rtol
-
-days_since_equinox = 85
 γ = 23.44
 ϖ = 282.95
 e = 0.017
+
+days_since_equinox = 85
+
+S = instantaneous_insolation(date, tz, lon, lat)
+@test S ≈ 1057.824 rtol=rtol
+
+S = daily_insolation(date, lat)
+@test S ≈ 338.117 rtol=rtol
+
+θ = 58.0
+d = 1.50e11 
+S = insolation(θ, d)
+@test S ≈ 161.454 rtol=rtol
+
 S = daily_insolation(days_since_equinox, γ, ϖ, e, lat)
-@test S ≈ 25.0 rtol=rtol
+@test S ≈ 482.404 rtol=rtol
 
 ϖ = 282.95 + 180.0
 S = daily_insolation(days_since_equinox, γ, ϖ, e, lat)
-@test S ≈ 25.0 rtol=rtol
+@test S ≈ 515.197 rtol=rtol
 
 γ = 22.0
 ϖ = 282.95
 S = daily_insolation(days_since_equinox, γ, ϖ, e, lat)
-@test S ≈ 25.0 rtol=rtol
+@test S ≈ 473.324 rtol=rtol
 
 γ = 18.0
 S = daily_insolation(days_since_equinox, γ, ϖ, e, lat)
-@test S ≈ 25.0 rtol=rtol
+@test S ≈ 447.527 rtol=rtol
 
 γ = 60.0
 S = daily_insolation(days_since_equinox, γ, ϖ, e, lat)
-@test S ≈ 25.0 rtol=rtol
+@test S ≈ 728.479 rtol=rtol
 
 γ = 97.86
 S = daily_insolation(days_since_equinox, γ, ϖ, e, lat)
-@test S ≈ 25.0 rtol=rtol
-
-θ = 35.0
-d = 1.496e11 
-S = insolation(θ, d)
-@test S ≈ 25.0 rtol=rtol
+@test S ≈ 833.273 rtol=rtol
--- test/test_zenith_angle.jl
+++ test/test_zenith_angle.jl
@@ -10,39 +10,46 @@ tz = PST
 lon, lat = California
 
 date = Dates.DateTime(2020,3,21,12,0,0)
-sza = instantaneous_zenith_angle(date, tz, lon, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
 
 γ = 23.44
 ϖ = 282.95
 e = 0.017
-sza = instantaneous_zenith_angle(date, tz, lon, lat, γ, ϖ, e)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
-
-sza = daily_zenith_angle(date, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
 
 days_since_equinox = 85
-sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
+
+sza, d = instantaneous_zenith_angle(date, tz, lon, lat)
+@test rad2deg(sza) ≈ 39.573 rtol=rtol
+
+sza, d = instantaneous_zenith_angle(date, tz, lon, lat, γ, ϖ, e)
+@test rad2deg(sza) ≈ 76.718 rtol=rtol
+
+sza, d = daily_zenith_angle(date, lat)
+@test rad2deg(sza) ≈ 75.737 rtol=rtol
+
+sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
+@test rad2deg(sza) ≈ 68.559 rtol=rtol
 
 ϖ = 282.95 + 180.0
 sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
+@test rad2deg(sza) ≈ 68.982 rtol=rtol
 
 γ = 22.0
 ϖ = 282.95
-sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
+sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
+println(rad2deg(sza))
+@test rad2deg(sza) ≈ 68.982 rtol=rtol
 
 γ = 18.0
-sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
+sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
+println(rad2deg(sza))
+@test rad2deg(sza) ≈ 70.177 rtol=rtol
 
 γ = 60.0
-sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
+sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
+println(rad2deg(sza))
+@test rad2deg(sza) ≈ 56.495 rtol=rtol
 
 γ = 97.86
-sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 20.0 rtol=rtol
+sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
+println(rad2deg(sza))
+@test rad2deg(sza) ≈ 50.846 rtol=rtol
