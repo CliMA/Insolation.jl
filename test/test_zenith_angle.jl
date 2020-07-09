a6e09f0d6bbd5c91a9583e173e7252bd17156ab4
claresinger <csinger@caltech.edu>
2020-07-08 23:29:38 -0700

    fix syntax in test. all tests pass locally.

--- test/test_zenith_angle.jl
+++ test/test_zenith_angle.jl
@@ -30,26 +30,22 @@ sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
 @test rad2deg(sza) ≈ 68.559 rtol=rtol
 
 ϖ = 282.95 + 180.0
-sza = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-@test rad2deg(sza) ≈ 68.982 rtol=rtol
+sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
+@test rad2deg(sza) ≈ 68.524 rtol=rtol
 
 γ = 22.0
 ϖ = 282.95
 sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-println(rad2deg(sza))
 @test rad2deg(sza) ≈ 68.982 rtol=rtol
 
 γ = 18.0
 sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-println(rad2deg(sza))
 @test rad2deg(sza) ≈ 70.177 rtol=rtol
 
 γ = 60.0
 sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-println(rad2deg(sza))
 @test rad2deg(sza) ≈ 56.495 rtol=rtol
 
 γ = 97.86
 sza, d = daily_zenith_angle(days_since_equinox, γ, ϖ, e, lat)
-println(rad2deg(sza))
 @test rad2deg(sza) ≈ 50.846 rtol=rtol
