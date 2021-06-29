5bc4a83e280b32b766a1f8e41a097374bd1b2eb9
claresinger <csinger@caltech.edu>
2021-06-29 14:02:10 -0700

    add more tests and increase code coverage

--- test/test_zenith_angle.jl
+++ test/test_zenith_angle.jl
@@ -9,14 +9,40 @@ const param_set = EarthParameterSet()
 rtol = 1e-2
 
 ## Test Zenith
+# sunrise at equator
 date = Dates.DateTime(2020, 1, 1, 6, 0, 0)
 lon, lat = [0.0, 0.0]
 sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
 @test sza ≈ π/2 rtol=rtol
 
+# sunset at equator
 date = Dates.DateTime(2020, 1, 1, 18, 0, 0)
 sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
 @test sza ≈ π/2 rtol=rtol
+
+# polar night NH 1
+date = Dates.DateTime(2020, 12, 20, 11, 0, 0)
+lon, lat = [0.0, 80.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+@test sza > π/2
+
+# polar night NH 2
+date = Dates.DateTime(2020, 12, 20, 23, 0, 0)
+lon, lat = [0.0, 80.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+@test sza > π/2
+
+# polar night SH 1
+date = Dates.DateTime(2020, 6, 20, 11, 0, 0)
+lon, lat = [0.0, -80.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+@test sza > π/2
+
+# polar night SH 2
+date = Dates.DateTime(2020, 6, 20, 23, 0, 0)
+lon, lat = [0.0, -80.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+@test sza > π/2
 
 ## Test Azimuth
 date = Dates.DateTime(2020, 1, 1, 0, 0, 0)
