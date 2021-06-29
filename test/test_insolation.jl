e398ed8c167b8c1d024ac532a53fa60ee53e9894
claresinger <csinger@caltech.edu>
2021-06-29 14:27:54 -0700

    add more tests and increase code coverage

--- test/test_insolation.jl
+++ test/test_insolation.jl
@@ -1,6 +1,48 @@
 using Statistics
+using Dates
+using Insolation 
 
+using CLIMAParameters
+using CLIMAParameters.Planet
+struct EarthParameterSet <: AbstractEarthParameterSet end
+const param_set = EarthParameterSet()
+
+atol = 1e-6
 rtol = 1e-2
+
+## Test zero insolation at night
+# sunrise at equator
+date = Dates.DateTime(2020, 1, 1, 6, 0, 0)
+lon, lat = [0.0, 0.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+F = insolation(sza, d, param_set)
+@test F ≈ 0.0 atol=atol
+
+S, mu = solar_flux_and_cos_sza(date, lon, lat, param_set)
+@test S ≈ tot_solar_irrad(param_set) rtol=4e-2
+@test mu ≈ 0.0 atol=atol
+
+# polar night NH 1
+date = Dates.DateTime(2020, 12, 20, 11, 0, 0)
+lon, lat = [0.0, 80.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+F = insolation(sza, d, param_set)
+@test F ≈ 0.0 atol=atol
+
+S, mu = solar_flux_and_cos_sza(date, lon, lat, param_set)
+@test S ≈ tot_solar_irrad(param_set) rtol=4e-2
+@test mu ≈ 0.0 atol=atol
+
+# polar night NH 2
+date = Dates.DateTime(2020, 12, 20, 23, 0, 0)
+lon, lat = [0.0, 80.0]
+sza, azi, d = instantaneous_zenith_angle(date, lon, lat, param_set)
+F = insolation(sza, d, param_set)
+@test F ≈ 0.0 atol=atol
+
+S, mu = solar_flux_and_cos_sza(date, lon, lat, param_set)
+@test S ≈ tot_solar_irrad(param_set) rtol=4e-2
+@test mu ≈ 0.0 atol=atol
 
 ## Test globally averaged insolation ≈ TSI
 ndays, nlats = [365, 181]
@@ -20,6 +62,8 @@ zonal_mean_insol = mean(F_arr, dims=1)
 area_fac = abs.(cosd.(l_arr))
 global_mean_insol = sum(zonal_mean_insol * area_fac) / sum(area_fac)
 @test global_mean_insol ≈ tot_solar_irrad(param_set) / 4 rtol=rtol
+
+
 
 ## Test symmetry of insolation at equinox
 date = Dates.DateTime(2021,3,20,9,34)
