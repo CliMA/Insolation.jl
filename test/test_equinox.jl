4ba99ecec1899a0f1dd8c07ce567d1ce8f11e3e7
claresinger <csinger@caltech.edu>
2022-04-25 18:31:49 -0700

    working on tests

--- test/test_equinox.jl
+++ test/test_equinox.jl
@@ -1,5 +1,6 @@
 push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 
+using Statistics
 using Roots
 using Dates
 using Insolation 
@@ -9,20 +10,21 @@ using CLIMAParameters
 using CLIMAParameters.Planet
 struct EarthParameterSet <: AbstractEarthParameterSet end
 const param_set = EarthParameterSet()
+#CLIMAParameters.Planet.lon_perihelion(::EarthParameterSet) = deg2rad(282.937348 + 180)
 
 atol = 1e-6
 rtol = 1e-2
 
+# Difference in NH and SH zenith angles at time x in given year
 function zdiff(x, year)
-    # Difference in NH and SH zenith angles at time x in given year
     date = xtodate(x,year)
     theta_s, dist = daily_zenith_angle(date, -45., param_set)
     theta_n, dist = daily_zenith_angle(date, 45., param_set)
     return theta_n - theta_s
 end
 
+# x is date relative to March 1, with 1.00 representing March 1 00:00
 function xtodate(x, year)
-    # x is date relative to March 1, with 1.00 representing March 1 00:00
     basedate = Dates.DateTime(year, 3, 1)
     deltat = Dates.Second(round((x-1)*86400))
     date = basedate + deltat
@@ -31,10 +33,18 @@ end
 
 day = zeros(length(1900:2100))
 for (i,year) in enumerate(1900:2100)
-    # Temporary function to allow passing the year argument
     f = (x -> zdiff(x, year))
+    # println(f(1), f(15), f(30))
     day[i] = find_zeros(f,1.,30)[1]
-    #@printf("%d  %6.3f  %s\n", year, d[1], Dates.format(xtodate(d[1], year), "yyyy-mm-dd HH:MM"))
 end
 
-plot(1900:2100, day)
+# plot(1900:2100, day)
+# xlabel!("Year")
+# ylabel!("Day in March")
+# title!("Date of vernal equinox")
+
+# test mean is about March 21
+@test mean(day) ≈ 21 atol=0.5
+
+test decreasing
+@test mean(day[:100]) > mean(day[100:end])
--- test/test_perihelion.jl
+++ test/test_perihelion.jl
@@ -1,5 +1,6 @@
 push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 
+using Statistics
 using Optim
 using Dates
 using Insolation 
@@ -9,18 +10,20 @@ using CLIMAParameters
 using CLIMAParameters.Planet
 struct EarthParameterSet <: AbstractEarthParameterSet end
 const param_set = EarthParameterSet()
+#CLIMAParameters.Planet.lon_perihelion(::EarthParameterSet) = deg2rad(282.937348 + 180)
 
 atol = 1e-6
 rtol = 1e-2
 
+# x is date relative to Jan 1, with 1.00 representing Jan 1 00:00
 function xtojandate(x, year)
-    # x is date relative to Jan 1, with 1.00 representing Jan 1 00:00
     basedate = Dates.DateTime(year, 1, 1)
     deltat = Dates.Second(round((x-1)*86400))
     date = basedate + deltat
     return date
 end
 
+# Earth-Sun distance
 function edist(x, year)
     date = xtojandate(x,year)
     theta, dist = daily_zenith_angle(date, 0., param_set)
@@ -30,11 +33,20 @@ end
 years = 1900:2100
 day = zeros(length(years))
 for (i,year) in enumerate(years)
-    # Temporary function to allow passing the year argument
     f = (x -> edist(x, year))
     res = optimize(f,1.,30)
+    #println(f(1), f(20), f(360))
     day[i] = Optim.minimizer(res)[1]
-    # @printf("%d  %6.3f  %s\n", year, d[1], Dates.format(xtojandate(d[1], year), "yyyy-mm-dd HH:MM"))
 end
 
-plot((years), day)
+# #print(day)
+# plot((years), day)
+# xlabel!("Year")
+# ylabel!("Day in Jan")
+# title!("Date of perihelion")
+
+# test mean is about Jan 3.5
+@test mean(day) ≈ 3.5 atol=0.5
+
+# test increasing
+@test mean(day[:100]) < mean(day[100:end])
