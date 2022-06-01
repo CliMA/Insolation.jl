6809adf48cda8bb2f7cf4f4575e5921a4ff02111
claresinger <clareees@gmail.com>
2022-06-01 14:46:45 -0700

    add Milankovitch cycles; add tests for long-term variations in orbital parameters; update docs accordingly; make dates work with Float32; update equations to not reference mean anomaly to vernal equinox but rather do it directly to the epoch.

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,15 +9,35 @@ using Roots
 using Optim
 
 using CLIMAParameters
-using CLIMAParameters.Planet: orbit_semimaj, tot_solar_irrad
+using CLIMAParameters.Planet#: orbit_semimaj, tot_solar_irrad
 struct EarthParameterSet <: AbstractEarthParameterSet end
 const param_set = EarthParameterSet()
 
+FT = Float32
+@testset "Orbital Params" begin
+    include("test_orbit_param.jl")
+end
 @testset "Types" begin
     include("test_types.jl")
 end
+@testset "Zenith Angle" begin
+    include("test_zenith_angle.jl")
+end
+@testset "Insolation" begin
+    include("test_insolation.jl")
+end
+@testset "Orbits" begin
+    include("test_perihelion.jl")
+    include("test_equinox.jl")
+end
 
 FT = Float64
+@testset "Orbital Params" begin
+    include("test_orbit_param.jl")
+end
+@testset "Types" begin
+    include("test_types.jl")
+end
 @testset "Zenith Angle" begin
     include("test_zenith_angle.jl")
 end
